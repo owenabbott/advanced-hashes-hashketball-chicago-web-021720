9083a9af1ae579db5fe6862cd7166b7fb83457a3
Owen Abbott <ocabbott@gmail.com>
2020-02-10 02:49:50 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -170,7 +170,7 @@ def player_numbers(team_name)
     if team[:team_name]==team_name
       team.each do |key, value|
         if key == :players
-          value.map[:number]
+          key.map[:number]
         end
       end
     end
