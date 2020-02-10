cdf8fa30c73e0bd5ddaa7220756a0df421e30595
Owen Abbott <ocabbott@gmail.com>
2020-02-10 02:47:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,5 +1,181 @@
 # Write your code here!
+# falling behind so I have to use the walkthrough video for this one. but I know I can do this on my own.
 
+
+
+#i'm watching the walkthrough and I fully realize that I should build a method before all of this targetting whatever key, which would eliminate the repetitiveness of some of these methods.
+
+def game_hash
+  {
+    :away => {:team_name => "Charlotte Hornets",
+    :colors => ["Turquoise", "Purple"],
+    :players => [
+      {:player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10, 
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7, 
+        :slam_dunks => 2
+        
+      },
+      {:player_name => "Bismack Biyombo",
+      :number => 0,
+      :shoe => 16,
+      :points => 12,
+      :rebounds => 4, 
+      :assists => 7,
+      :steals => 22,
+      :blocks => 15,
+      :slam_dunks => 10
+      },
+      {:player_name => "DeSagna Diop",
+      :number => 2,
+      :shoe => 14,
+      :points => 24,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 4,
+      :blocks => 5,
+      :slam_dunks => 5
+      },
+      {:player_name => "Ben Gordon",
+      :number => 8,
+      :shoe => 15,
+      :points => 33,
+      :rebounds => 3,
+      :assists => 2,
+      :steals => 1,
+      :blocks => 1,
+      :slam_dunks => 0
+      },
+      {:player_name => "Kemba Walker",
+      :number => 33,
+      :shoe => 15,
+      :points => 6,
+      :rebounds => 12,
+      :assists => 2,
+      :steals => 7,
+      :blocks => 5,
+      :slam_dunks => 12}]
+    
+  },
+    :home => {:team_name => "Brooklyn Nets",
+    :colors => ["Black", "White"],
+    :players => [{
+      :player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds => 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1
+    },
+    {:player_name => "Reggie Evans",
+    :number => 30,
+    :shoe => 14,
+    :points => 12,
+    :rebounds => 12,
+    :assists => 12,
+    :steals => 12,
+    :blocks => 12,
+    :slam_dunks => 7
+    },
+    {:player_name => "Brook Lopez",
+    :number => 11,
+    :shoe => 17,
+    :points => 17,
+    :rebounds => 19,
+    :assists => 10,
+    :steals => 3,
+    :blocks => 1,
+    :slam_dunks => 15
+  },
+  {:player_name => "Mason Plumlee",
+  :number => 1,
+  :shoe => 19,
+  :points => 26,
+  :rebounds => 11,
+  :assists => 6,
+  :steals => 3,
+  :blocks => 8,
+  :slam_dunks => 5
+  },
+  {:player_name => "Jason Terry",
+  :number => 31,
+  :shoe => 15,
+  :points => 19,
+  :rebounds => 2,
+  :assists => 2,
+  :steals => 4,
+  :blocks => 11,
+  :slam_dunks => 1}]
+      
+    }}
+    
+    #why is this even a method? wouldn't it have been better as just a variable defined as the hash? 
+  
+end
+
+def num_points_scored(name)
+  #returns num of points for player passed in
+  game_hash.each do |place, team|
+    team.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if player[:player_name] == name
+            return player[:points]
+        end
+      end
+    end
+  end
+end
+end 
+
+def shoe_size(name)
+    game_hash.each do |place, team|
+    team.each do |key, value|
+      if key == :players
+        value.each do |player|
+          if player[:player_name] == name
+            return player[:shoe]
+        end
+      end
+    end
+  end
+end
+end
+
+def team_colors(the_team)
+  game_hash.each do |place, team|
+    if the_team == team[:team_name]
+      return team[:colors]
+    end
+  end
+ end
+ 
+
+def team_names
+  game_hash.map do |place, team|
+    team[:team_name]
+  end
+end
+
+def player_numbers(team_name)
+  game_hash.map do |place, team|
+    if team[:team_name]==team_name
+      team.each do |key, value|
+        if key == :players_name
+          key[:number]
+        end
+      end
+    end
+  end
+end
 
 
 
